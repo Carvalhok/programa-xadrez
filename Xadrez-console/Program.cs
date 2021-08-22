e54656cc76b564987eab1759bb49693867dc29c2
Keverson Carvalho <kalmeida743@gmail.com>
2021-08-22 10:34:35 -0300

    Duas frases implementadas

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -7,14 +7,8 @@ namespace Xadrez_console
     {
        static void Main(string[] args)
         {
-            Account acc1 = new Account(1234, "Keverson", 500.0);
-            Account acc2 = new BusinessAccount(4321, "Leila", 500.0, 200.0);
-
-            acc1.Withhdraw(10.0);
-            acc2.Withhdraw(10.0);
-
-            Console.WriteLine("Saque na classe Account: " + acc1.Balance);
-            Console.WriteLine("Saque na classe BusinessAccount: " + acc2.Balance);
+            Console.WriteLine("Olá mundo");
+            Console.WriteLine("Como vc está hoje");
         }
 
         
